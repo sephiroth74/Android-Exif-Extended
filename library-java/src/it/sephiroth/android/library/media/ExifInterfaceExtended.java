6982ff34bc76c916f8a7b2e0b1db2723756f1483
Alessandro Crugnola <alessandro@aviary.com>
2014-04-09 22:47:27 -0400

    updated version

--- library-java/src/it/sephiroth/android/library/media/ExifInterfaceExtended.java
+++ library-java/src/it/sephiroth/android/library/media/ExifInterfaceExtended.java
@@ -46,7 +46,7 @@ import android.util.Log;
 @SuppressLint("DefaultLocale")
 public class ExifInterfaceExtended {
 
-	public static final String VERSION = "2.97.4-SNAPSHOT";
+	public static final String VERSION = "2.97.4";
 
 	private static final String LOG_TAG = "ExifInterfaceExtended";
 
